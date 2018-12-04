2da0f05b3c55ada13c304396201c0d5dee8a4832
reck1ess <heeinso@snu.ac.kr>
2018-12-04 23:22:19 +0900

    feat: Add AppLayout component

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 
+import AppLayout from './AppLayout';
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<AppLayout>
 				<WelcomeMessage />
-			</div>
+			</AppLayout>
 		);
 	}
 }
