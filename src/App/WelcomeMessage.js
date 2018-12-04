977d26311b83649f48b8a1e307fe273ef8e782be
reck1ess <heeinso@snu.ac.kr>
2018-12-04 23:16:44 +0900

    feat: Restructure files on boilerplate

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WelcomeMessage = () => {
+	return <div>Welcome to Cryptodash</div>;
+};
+
+export default WelcomeMessage;
---
+++ src/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import WelcomeMessage from './WelcomeMessage';
+
+class App extends Component {
+	render() {
+		return <WelcomeMessage />;
+	}
+}
+
+export default App;
