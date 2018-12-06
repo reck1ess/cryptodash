df08602e1f31d1b2c088f895010ab18dab727c6a
reck1ess <heeinso@snu.ac.kr>
2018-12-06 23:13:26 +0900

    chore: Add a cryptocompare

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const cc = require('cryptocompare');
+
 export const AppContext = React.createContext();
 
 export class AppProvider extends Component {
@@ -14,6 +16,15 @@ export class AppProvider extends Component {
 	}
 
 	setPage = page => this.setState({ page });
+
+	componentDidMount = () => {
+		this.fetchCoins();
+	};
+
+	fetchCoins = async () => {
+		let coinList = (await cc.coinList()).Data;
+		this.setState({ coinList });
+	};
 
 	confirmFavorites = () => {
 		this.setState({
