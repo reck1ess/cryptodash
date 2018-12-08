d805e87c331bbfe90189423902e7585c9fd186ea
reck1ess <heeinso@snu.ac.kr>
2018-12-08 20:45:27 +0900

    feat: Add a Search component

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,12 +3,14 @@ import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import CoinGrid from './CoinGrid';
 import Page from '../Shared/Page';
+import Search from './Search';
 
 export default () => (
 	<Page name="Settings">
 		<WelcomeMessage />
 		<CoinGrid topSection />
 		<ConfirmButton />
+		<Search />
 		<CoinGrid />
 	</Page>
 );
